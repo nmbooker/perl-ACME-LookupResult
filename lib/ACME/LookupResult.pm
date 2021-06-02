808c1c226373a70aba79036359a6e03e3a153ee8
Nick Booker <nick.booker@opusvl.com>
2021-06-02 11:35:53 +0100

    Add boilerplate and document basic reason for existance

--- lib/ACME/LookupResult.pm
+++ lib/ACME/LookupResult.pm
@@ -1,5 +1,25 @@
+package ACME::LookupResult;
+# ABSTRACT: A DSL to flexibly handle success or failure of a lookupo
+
+=head1 DESCRIPTION
+
+Sometimes it's hard to choose what to do when someone calls one of your methods
+asking for a specific object and you don't have that object.
+
+Now you don't.  All you need to do is ensure that all non-exceptional branches
+in your method return either C<found($object)> or C<not_found()>, document that
+that's what you've done and leave it to your client to decide whether it's
+fatal and how to check.
+
+The only prereqisite on your client is that they do make a choice to handle
+the error by either checking for success (C< Found >), asking for an empty list or undef in the not found case (C<Slip> or C<OrUndef> respectively) or just
+asking for (therefore assuming presence of) the C<Value> which will throw an
+exception in the C<not_found> case.
+
+=cut
+
+use v5.28;
 use strict;
 use warnings;
-package ACME::LookupResult;
 
 1;
